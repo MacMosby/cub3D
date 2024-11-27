12997e2fa2590a818730ff0d2a7122ebcfbe8180
Marc Rodenbusch <mrodenbu@student.42berlin.de>
2024-11-27 12:38:33 +0100

    wall collision protection and movement improvement

--- player_movement.c
+++ player_movement.c
@@ -12,32 +12,36 @@
 
 # include "cub3D.h"
 
+int	is_pos_valid(t_data *data, double pos_x, double pos_y)
+{
+	int	grid_x;
+	int	grid_y;
+
+	grid_x = floor(pos_x / (double)CUBE_SIZE);
+	grid_y = floor(pos_y / (double)CUBE_SIZE);
+
+	if (data->map[grid_y][grid_x] != '1')
+		return (1);
+	else
+		return (0);
+}
+
 void	move_forward(t_data *data)
 {
 	double	dX;
 	double	dY;
 
+	dX = 0;
+	dY = 0;
 	printf("move forward\n");
 	if (data->player->angle == 0)
-	{
-		dX = 1;
-		dY = 0;
-	}
+		dX = 1 * MOVE_SPEED;
 	else if (data->player->angle == 90)
-	{
-		dX = 0;
-		dY = 1;
-	}
+		dY = 1 * MOVE_SPEED;
 	else if (data->player->angle == 180)
-	{
-		dX = -1;
-		dY = 0;
-	}
+		dX = -1 * MOVE_SPEED;
 	else if (data->player->angle == 270)
-	{
-		dX = 0;
-		dY = -1;
-	}
+		dY = -1 * MOVE_SPEED;
 	else
 	{
 		dX = MOVE_SPEED * cos(data->player->angle / (double)180 * M_PI);
@@ -45,9 +49,12 @@ void	move_forward(t_data *data)
 	}
 	printf("dY: %f\n", dY);
 	// don't forget to check for walls
-	data->player->position->x += dX;
-	data->player->position->y += dY;
-	ray_caster(data);
+	if (is_pos_valid(data, data->player->position->x + dX, data->player->position->y - dY))
+	{
+		data->player->position->x += dX;
+		data->player->position->y -= dY;
+		ray_caster(data);
+	}
 }
 
 void	move_backward(t_data *data)
@@ -55,16 +62,30 @@ void	move_backward(t_data *data)
 	double	dX;
 	double	dY;
 
+	dX = 0;
+	dY = 0;
 	printf("move backward\n");
-	dX = MOVE_SPEED * cos(data->player->angle / (double)180 * M_PI);
-	//dX = 0;
-	dY = MOVE_SPEED * sin(data->player->angle / (double)180 * M_PI);
-	//dY = 0;
+	if (data->player->angle == 0)
+		dX = 1 * MOVE_SPEED;
+	else if (data->player->angle == 90)
+		dY = 1 * MOVE_SPEED;
+	else if (data->player->angle == 180)
+		dX = -1 * MOVE_SPEED;
+	else if (data->player->angle == 270)
+		dY = -1 * MOVE_SPEED;
+	else
+	{
+		dX = MOVE_SPEED * cos(data->player->angle / (double)180 * M_PI);
+		dY = MOVE_SPEED * sin(data->player->angle / (double)180 * M_PI);
+	}
 	printf("dY: %f\n", dY);
 	// don't forget to check for walls
-	data->player->position->x -= dX;
-	data->player->position->y -= dY;
-	ray_caster(data);
+	if (is_pos_valid(data, data->player->position->x - dX, data->player->position->y + dY))
+	{
+		data->player->position->x -= dX;
+		data->player->position->y += dY;
+		ray_caster(data);
+	}
 }
 
 void	move_left(t_data *data)
@@ -73,16 +94,32 @@ void	move_left(t_data *data)
 	double	dY;
 	double	angle;
 
-	printf("move left\n");
 	angle = data->player->angle + 90;
 	if (angle > 360)
 		angle -= 360;
-	dX = MOVE_SPEED * cos(angle / (double)180 * M_PI);
-	dY = MOVE_SPEED * sin(angle / (double)180 * M_PI);
+	dX = 0;
+	dY = 0;
+	printf("move left\n");
+	if (angle == 0)
+		dX = 1 * MOVE_SPEED;
+	else if (angle == 90)
+		dY = 1 * MOVE_SPEED;
+	else if (angle == 180)
+		dX = -1 * MOVE_SPEED;
+	else if (angle == 270)
+		dY = -1 * MOVE_SPEED;
+	else
+	{
+		dX = MOVE_SPEED * cos(angle / (double)180 * M_PI);
+		dY = MOVE_SPEED * sin(angle / (double)180 * M_PI);
+	}
 	// don't forget to check for walls
-	data->player->position->x += dX;
-	data->player->position->y += dY;
-	ray_caster(data);
+	if (is_pos_valid(data, data->player->position->x + dX, data->player->position->y - dY))
+	{
+		data->player->position->x += dX;
+		data->player->position->y -= dY;
+		ray_caster(data);
+	}
 }
 
 void	move_right(t_data *data)
@@ -91,16 +128,32 @@ void	move_right(t_data *data)
 	double	dY;
 	double	angle;
 
-	printf("move right\n");
 	angle = data->player->angle - 90;
 	if (angle < 0)
 		angle += 360;
-	dX = MOVE_SPEED * cos(angle / (double)180 * M_PI);
-	dY = MOVE_SPEED * sin(angle / (double)180 * M_PI);
+	dX = 0;
+	dY = 0;
+	printf("move right\n");
+	if (angle == 0)
+		dX = 1 * MOVE_SPEED;
+	else if (angle == 90)
+		dY = 1 * MOVE_SPEED;
+	else if (angle == 180)
+		dX = -1 * MOVE_SPEED;
+	else if (angle == 270)
+		dY = -1 * MOVE_SPEED;
+	else
+	{
+		dX = MOVE_SPEED * cos(angle / (double)180 * M_PI);
+		dY = MOVE_SPEED * sin(angle / (double)180 * M_PI);
+	}
 	// don't forget to check for walls
-	data->player->position->x += dX;
-	data->player->position->y += dY;
-	ray_caster(data);
+	if (is_pos_valid(data, data->player->position->x + dX, data->player->position->y - dY))
+	{
+		data->player->position->x += dX;
+		data->player->position->y -= dY;
+		ray_caster(data);
+	}
 }
 
 void	turn_left(t_data *data)
