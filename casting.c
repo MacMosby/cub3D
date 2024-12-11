9c596c73a918b34ea643e8a693a416bf3ace98e4
Marc Rodenbusch <mrodenbu@student.42berlin.de>
2024-12-11 18:49:16 +0100

    fixed wall texture mapping

--- casting.c
+++ casting.c
@@ -24,7 +24,7 @@ double	get_correct_distance(double hori_dist, double vert_dist, double angle)
 		return (vert_dist * cos(angle / (double)180 * M_PI));
 }
 
-void	cast_slice(t_data *data, double wall_distance, int col, int offset)
+void	cast_slice(t_data *data, double wall_distance, int col, int offset, int wall)
 {
 	int i = 0;
 	int	slice_height;
@@ -42,10 +42,16 @@ void	cast_slice(t_data *data, double wall_distance, int col, int offset)
 	}
 	// drawing the wall
 	// printf("offset: %d\n", offset);
-	offset += 0;
 	while (i < HEIGHT - ceiling_floor_height)
 	{
-		my_pixel_put(col, i, &data->imag, SANDY_YELLOW);
+		if (wall == E)
+			my_pixel_put(col, i, &data->imag, *(unsigned int *)(data->ea_img.pixels_ptr + ((i - ceiling_floor_height) * CUBE_SIZE / slice_height * data->ea_img.line_len) + (offset * data->ea_img.bpp / 8)));
+		else if (wall == N)
+			my_pixel_put(col, i, &data->imag, *(unsigned int *)(data->no_img.pixels_ptr + ((i - ceiling_floor_height) * CUBE_SIZE / slice_height * data->no_img.line_len) + (offset * data->no_img.bpp / 8)));
+		else if (wall == W)
+			my_pixel_put(col, i, &data->imag, *(unsigned int *)(data->we_img.pixels_ptr + ((i - ceiling_floor_height) * CUBE_SIZE / slice_height * data->we_img.line_len) + (offset * data->we_img.bpp / 8)));
+		else if (wall == S)
+			my_pixel_put(col, i, &data->imag, *(unsigned int *)(data->so_img.pixels_ptr + ((i - ceiling_floor_height) * CUBE_SIZE / slice_height * data->so_img.line_len) + (offset * data->so_img.bpp / 8)));
 		i++;
 	}
 	// drawing the floor
